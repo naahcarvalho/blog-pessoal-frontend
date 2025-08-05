9a91537f35bd6ede44c1e31c34e35d0dfcbf7ed1
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-05 08:28:04 -0300

    feat: atualizando App, navbar e página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import './App.css'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
